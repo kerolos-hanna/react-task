966d74061842dbfb8f8d27d08cdf21c1dbb4ae32
kerolos hanna saad <https://github.com/kerolos-hanna>
2020-06-12 02:58:54 +0200

    add spinner and some detail in cardDetails

--- src/pages/CardDetails/CardDetails.js
+++ src/pages/CardDetails/CardDetails.js
@@ -1,4 +1,6 @@
-import React, {useState, useEffect} from 'react';
+/** @format */
+
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Typography from '@material-ui/core/Typography';
@@ -6,67 +8,84 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
+import Rating from '@material-ui/lab/Rating';
 
 import { API_KEY } from '../../API_KEY/API_KEY';
+import Spinner from '../../Component/UI/Spinner/Spinner';
 import useStyles from './CardDetailsStyle';
 
-
 const CardDetails = (props) => {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const [data, setData] = useState(null);
+	const [data, setData] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    console.log(props.match.params.id)
-    console.log(API_KEY)
-    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/${props.match.params.id}`, {
-        headers: {
-          Authorization: `Bearer ${API_KEY}`
-        },
-        params: {
-          // categories: "coffee",
-        }
-      })
-      .then((res) => {
-        setData(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  },[props]);
+	useEffect(() => {
+		axios
+			.get(
+				`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/${
+					props.match.params.id
+				}`,
+				{
+					headers: {
+						Authorization: `Bearer ${API_KEY}`,
+					},
+				}
+			)
+			.then((res) => {
+				setData(res.data);
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [props.match]);
 
-  return (
-    <Grid container justify="center">
-        <Grid item lg={6} md={8} xs={12}>
-          <Card className={classes.cardImage}>
-            <CardMedia
-              component="img"
-              image={data?.image_url}
-            />
-          </Card>
-        </Grid>
-        <Grid item lg={6} md={8} xs={10}>
-          <Grid container spacing={4} justify="center">
-            <Paper
-              className={!props.matches ? classes.paper : classes.paperSmallScreen}
-            >
-              <Typography variant="h4" component="h1" gutterBottom>
-                {data?.name}
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                {data?.price}
-              </Typography>
+	let loadingData = <Spinner />;
 
-              <hr />
-              <Typography variant="body1" gutterBottom>
-                {data?.review_count}
-              </Typography>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Grid>
-  )
-}
+	if (!loading) {
+		loadingData = (
+			<Grid container justify="center">
+				<Grid item lg={6} md={8} xs={12}>
+					<Card className={classes.cardImage}>
+						<CardMedia component="img" image={data?.image_url} />
+					</Card>
+				</Grid>
+				<Grid item lg={6} md={8} xs={10}>
+					<Grid container spacing={4} justify="center">
+						<Paper
+							className={
+								!props.matches ? classes.paper : classes.paperSmallScreen
+							}
+						>
+							<Typography variant="h4" component="h1" gutterBottom>
+								Name: {data?.name}
+							</Typography>
+							<Typography variant="body1" gutterBottom>
+								Num. of review: {data?.review_count}
+							</Typography>
+							<Typography variant="body1" gutterBottom>
+								Phone: {data?.phone}
+							</Typography>
+							<Rating
+								name="half-rating-read"
+								defaultValue={data.rating}
+								precision={0.5}
+								readOnly
+							/>
+
+							<hr />
+							<Typography variant="body1" gutterBottom color="primary">
+								Price: {data?.price}
+							</Typography>
+						</Paper>
+					</Grid>
+				</Grid>
+			</Grid>
+		);
+	}
+
+	return loadingData;
+};
 
 export default CardDetails;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Search from '../../Component/Search/Search';
@@ -5,93 +7,101 @@ import DisplayData from '../../Component/DisplayData/DisplayData';
 import SortData from '../../Component/SortingData/SortingData';
 import SortFilterButtons from './SortFilterButtons/SortFilterButtons';
 import FilteredData from '../../Component/FilteredData/FilteredData';
+import Spinner from '../../Component/UI/Spinner/Spinner';
 import { API_KEY } from '../../API_KEY/API_KEY';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-
 import useStyles from './HomePageStyle';
 
 const Home = (props) => {
+	const classes = useStyles();
 
-  const classes = useStyles();
+	const [data, setData] = useState(null);
+	const [want, setWant] = useState(null);
+	const [location, setLocation] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [sort, setSort] = useState(false); //for sort of data(boolean)
+	const [filter, setFilter] = useState(null); //for value of select
 
-  const [data, setData] = useState(null);
-  const [want, setWant] = useState(null);
-  const [location, setLocation] = useState(null);
-  const [sort, setSort] = useState(false);    //for sort of data(boolean)
-  const [filter, setFilter] = useState(null); //for value of select
+	useEffect(() => {
+		if (want || location) {
+			axios
+				.get(
+					`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${location}`,
+					{
+						headers: {
+							Authorization: `Bearer ${API_KEY}`,
+						},
+						params: {
+							categories: `${want}`,
+						},
+					}
+				)
+				.then((res) => {
+					setData(res.data.businesses);
+					setLoading(false);
+				})
+				.catch((err) => {
+					setLoading(false);
+					console.log(err);
+				});
+		}
+	}, [want, location]);
 
+	const getData = (wan, loc) => {
+		setWant(wan);
+		setLocation(loc);
+		setLoading(true);
+	};
 
-  useEffect(() => {
-    if(want || location){
-      axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${location}`, {
-          headers: {
-            Authorization: `Bearer ${API_KEY}`
-          },
-          params: {
-            categories: `${want}`,
-          }
-        })
-          .then((res) => {
-            setData(res.data.businesses);
-            console.log(res.data.businesses);
-          })
-          .catch((err) => {
-            console.log(err)
-          })
-    }
-  }, [want, location]);
+	const SortHandler = () => {
+		setSort(!sort);
+	};
 
-  const getData = (wan, loc) => {
-    setWant(wan);
-    setLocation(loc);
-  }
+	const selectChangeHandler = (event) => {
+		setFilter(event.target.value);
+	};
 
-  const SortHandler = () => {
-    setSort(!sort);
-  }
+	let loadingData = (
+		<Grid item>
+			{data ? (
+				<React.Fragment>
+					<Typography className={classes.heroText} variant="h3">
+						Results
+					</Typography>
+					<SortFilterButtons
+						clicked={SortHandler}
+						sort={sort}
+						data={data}
+						filter={filter}
+						changed={selectChangeHandler}
+					/>
+				</React.Fragment>
+			) : null}
+			<Grid container spacing={4} justify="center" className={classes.list}>
+				{sort ? (
+					<SortData data={data} />
+				) : filter ? (
+					<FilteredData selectValue={filter} data={data} />
+				) : (
+					<DisplayData data={data} />
+				)}
+			</Grid>
+		</Grid>
+	);
 
-  const selectChangeHandler = (event) => {
-    setFilter(event.target.value)
-  }
+	if (loading) {
+		loadingData = <Spinner />;
+	}
 
-  return (
-    <div>
-      <Search clicked={getData}/>
-      <Grid item>
-        {(data)?
-          <React.Fragment>
-            <Typography className={classes.heroText} variant="h3">
-              Results
-            </Typography>
-            <SortFilterButtons 
-              clicked={SortHandler} 
-              sort={sort} 
-              data={data}
-              filter={filter}
-              changed={selectChangeHandler}
-            />
-          </React.Fragment>
-           : null
-        }
-        <Grid
-          container
-          spacing={4}
-          justify="center"
-          className={classes.list}
-        >
-          {
-            (sort)?
-            <SortData data={data}/> : (filter)? <FilteredData selectValue={filter} data={data}/> : <DisplayData data={data}/> 
-          }
-          
-        </Grid>
-      </Grid>
-
-    </div>
-  )
-}
+	return (
+		<div>
+			<Search clicked={getData} />
+			{loadingData}
+		</div>
+	);
+};
 
 export default Home;
